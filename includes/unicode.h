e3e6c3daf07ed1fd8497f48c8f1bd1b07c6e8e68
Jacob ZAK <jagu.sayan@gmail.com>
2014-03-11 13:46:08 +0000

    add support of unicode

---
+++ includes/unicode.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unicode.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jzak <jagu.sayan@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/10 23:32:04 by jzak              #+#    #+#             */
+/*   Updated: 2014/03/11 11:29:33 by jzak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UNICODE_H
+# define UNICODE_H
+
+typedef unsigned int	t_utf8;
+
+typedef struct			s_interval
+{
+	int					first;
+	int					last;
+}						t_interval;
+
+#endif /* !UNICODE_H */
